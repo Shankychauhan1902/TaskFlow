594adc47ae5ac6b7b8ceed13756e81b95b8d8987
Sankit Chauhan <sankit.khora@gmail.com>
2026-07-05 11:23:28 +0530

    Implement TaskFlow backend CRUD APIs

---
+++ server/controllers/taskController.js
@@ -0,0 +1,114 @@
+const Task = require("../models/task");
+const createTask = async (req, res) => {
+  try{
+    const { title, description, status, priority, dueDate } = req.body;
+    
+    if(!title || !title.trim()){
+      return res.status(400).json({
+        message: "Title is required",
+      });
+    }
+
+    const task = await Task.create({
+      title,
+      description,
+      status,
+      priority,
+      dueDate,
+    });
+
+    res.status(201).json(task);
+  }catch(error){
+    res.status(500).json({
+      message: "Failed to create task",
+      error: error.message,
+    });
+  }
+};
+
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  }
+  catch(error){
+    res.status(500).json({
+      message: "Failed to fetch tasks",
+      error: error.message,
+    });
+  }
+};
+
+const getTaskById = async (req, res) =>{
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if(!task){
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+
+    res.status(200).json(task);
+  }
+  catch(error){
+    res.status(500).json({
+      message: "Failed to fetch task",
+      error: error.message,
+    });
+  }
+};
+
+const updateTask = async (req, res) =>{
+  try{
+    const task = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    if(!task){
+      return res.status(404).json({
+        message: "Task not found",
+      });
+    }
+    res.status(200).json(task);
+  }catch(error){
+    res.status(500).json({
+      message: "Failed to update task",
+      error: error.message,
+    });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try{
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if(!task){
+      return res.status(404).json({
+        message: "task not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Task deleted successfully",
+    });
+  }
+  catch(error){
+    res.status(500).json({
+      message: "Failed to delete task",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createTask,
+  getTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+};
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv= require("dotenv");
+const connectDB= require("./config/db");
+const taskRoutes = require("./routes/taskRoutes");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use("/api/tasks",taskRoutes);
+
+
+app.get("/",(req, res)=> {
+  res.send("Welcome to TaskFlow API");
+});
+
+
+app.listen(PORT, ()=> {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
