9c129226c670dd23fd016765a603183b2e54708d
Sankit Chauhan <sankit.khora@gmail.com>
2026-07-08 23:18:14 +0530

    Configure CORS for frontend integration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv= require("dotenv");
+const cors = require("cors");
 const connectDB= require("./config/db");
 const taskRoutes = require("./routes/taskRoutes");
 
@@ -10,9 +11,10 @@ connectDB();
 const app = express();
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
-app.use("/api/tasks",taskRoutes);
 
+app.use("/api/tasks",taskRoutes);
 
 app.get("/",(req, res)=> {
   res.send("Welcome to TaskFlow API");
